ca08aac81fbdb419f516460c7f1b1e1398962dff
leunj8751 <leunj87511@gmail.com>
2024-08-12 00:23:40 +0900

    section2 Controller 생성

---
+++ src/main/java/com/eazybytes/EasyBankBackendApplication.java
@@ -0,0 +1,15 @@
+package com.eazybytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.eazybytes.controller") //Main 패키지의 하위 패키지에 컨트롤러가 존재 하지 않다면 이 어노테이션은 필수디
+public class EasyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBankBackendApplication.class, args);
+	}
+
+}
