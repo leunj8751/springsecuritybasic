28f6a67bf9f92ae16bbcfb6087a7abd20893cbdc
leunj8751 <leunj87511@gmail.com>
2024-08-17 14:48:46 +0900

    section3 커밋 - 데이터베이스 연결, EazyBankUserDetails 클래스 생성

---
+++ src/main/java/com/eazybytes/config/EazyBankUserDetails.java
@@ -0,0 +1,33 @@
+package com.eazybytes.config;
+
+
+import com.eazybytes.model.Customer;
+import com.eazybytes.repository.CustomerRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EazyBankUserDetails implements UserDetailsService {
+
+
+    private final CustomerRepository customerRepository;
+
+    public EazyBankUserDetails(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new
+                UsernameNotFoundException("User details not found for the user: " + username));
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+}
--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -5,7 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,15 +24,56 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests((requests) -> requests
-//            .anyRequest().denyAll()
-//            .anyRequest().permitAll()
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeRequests((requests) -> requests
             .requestMatchers("/myAcount","/myBalance","/myCards","/myLoans").authenticated()
-            .requestMatchers("/notices","/contact").permitAll()
+            .requestMatchers("/notices","/contact","/register").permitAll()
         );
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
 
+    /*
+    @Bean
+     public InMemoryUserDetailsManager userDetailsService() {
+
+         // use withDefaultPasswordEncoder
+//         UserDetails admin = User.withDefaultPasswordEncoder()
+//                 .username("admin")
+//                 .password("1234")
+//                 .authorities("admin")
+//                 .build();
+//         UserDetails user = User.withDefaultPasswordEncoder()
+//                 .username("user")
+//                 .password("1234")
+//                 .authorities("read")
+//                 .build();
+//         return new InMemoryUserDetailsManager(admin,user);
+
+        //use NoOpPasswordEncoder Bean
+         UserDetails admin = User.withUsername("admin")
+                 .password("1234")
+                 .authorities("admin")
+                 .build();
+         UserDetails user = User.withUsername("user")
+                 .password("1234")
+                 .authorities("read")
+                 .build();
+         return new InMemoryUserDetailsManager(admin,user);
+
+    }
+    */
+
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
