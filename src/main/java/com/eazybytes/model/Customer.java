ad03a77c2a8fd80447996bfef3db670da5aa1a43
leunj8751 <leunj87511@gmail.com>
2024-08-26 07:12:54 +0900

    CORS, CSRF 설정 및 CSRF토큰 구현
    (cherry picked from commit 8df0f31a9ecda3081ed988359fb0e00252e0d84b)

--- src/main/java/com/eazybytes/model/Customer.java
+++ src/main/java/com/eazybytes/model/Customer.java
@@ -1,8 +1,10 @@
 package com.eazybytes.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "customer")
@@ -11,12 +13,24 @@ import lombok.Setter;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    @Column(name = "customer_id")
+    private int id;
+
+    private String name;
+
     private String email;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String pwd;
-    @Column(name = "role")
+
     private String role;
 
+    @Column(name = "create_dt")
+    private String createDt;
 
 }
